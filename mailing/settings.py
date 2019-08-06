85eb5a26d4e0cc2025652dfafab62310d3f10642
rysbai <rysbai.coder@gmail.com>
2019-08-06 12:49:39 +0600

    Added eoifnweoifjwe

--- mailing/settings.py
+++ mailing/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-import djcelery
 from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
