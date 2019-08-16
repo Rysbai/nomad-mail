71ba7827c3dc49f02d7d911ce3a74a50db801369
rysbai <rysbai.coder@gmail.com>
2019-08-16 23:01:44 +0600

    Added coresheaders

--- mailing/settings.py
+++ mailing/settings.py
@@ -9,7 +9,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 INSTALLED_APPS = [
     'material.admin',
     'django.contrib.admin',
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'event.apps.EventConfig',
     'ckeditor_uploader',
     'ckeditor',
+    'corsheaders',
     'distribution.apps.DistributionConfig',
     'djcelery'
 ]
@@ -31,6 +32,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
